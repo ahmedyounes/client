d96ea8005978f325d3dfe30e2b51705c586efd88
Surya <surya@keyba.se>
2019-04-11 17:35:18 -0400

    sys

--- go/service/main.go
+++ go/service/main.go
@@ -1027,6 +1027,10 @@ func (d *Service) ConfigRPCServer() (net.Listener, error) {
 	return listener, nil
 }
 
+func (d *Service) Stop(exitCode keybase1.ExitCode) {
+	d.stopCh <- exitCode
+}
+
 func (d *Service) ListenLoopWithStopper(l net.Listener) (exitCode keybase1.ExitCode, err error) {
 	ch := make(chan error)
 	go func() {
